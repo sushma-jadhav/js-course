e79e59b02d449be72b8f0a96c3f3435baa4aba97
sushma-jadhav <59611909+sushma-jadhav@users.noreply.github.com>
2024-04-29 15:16:45 +0000

    comparison operators

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,17 @@
+// ***************** comparision operators ************* //
+// console.log(2 > 1)
+// console.log(2 < 1)
+// console.log(2 >= 1)
+// console.log(2 <= 1)
+// console.log(2 == 1)
+// console.log(2 != 1)
+
+// console.log("2" > 1);  string 2 is converted to number
+
+// console.log(null == 0); not to be used because null can be converted to anything may be 0 or NaN
+
+// console.log(undefined == 0) not recommended to use
+
+// *************** ==== *************** // Strict comparision checks strictly same data types 
+
+// null only equals undefined
